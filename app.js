293a5ae8642e240c90efc5826fc0a75137d9fb3c
smarika17 <malviyasmarika@gmail.com>
2023-03-04 22:34:48 +0530

    Database encryption and using enviornment variable to keep secret safe

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require ("express");
 const bodyParser = require("body-parser");
 const ejs= require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption")
 
 const app=express();
 
@@ -24,6 +26,8 @@ const userSchema =  new mongoose.Schema({
     password: String
 });
 
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 const User= new mongoose.model("User", userSchema);
 
 
